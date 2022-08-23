22ad9da65e8adbd5989a6e87512a50b5e1cd8235
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2022-08-23 14:51:43 +0430

    jasmine_sprout:  Adapt to LMODroid by device-add script
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: Ic75cf7e63a949c945784387ad28031f0c9631bc2

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_jasmine_sprout.mk
+    $(LOCAL_DIR)/lmodroid_jasmine_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_jasmine_sprout-user \
-    lineage_jasmine_sprout-userdebug \
-    lineage_jasmine_sprout-eng
+    lmodroid_jasmine_sprout-user \
+    lmodroid_jasmine_sprout-userdebug \
+    lmodroid_jasmine_sprout-eng
